2607bafaee46aaf5cd446107a3b3159c783f3cb1
Kristina Auckland <kristina.auckland@gmail.com>
2012-11-24 13:30:17 +0000

    Adding compass and sass to the starkers wordpress theme

--- development/www/wp-content/themes/starkers/parts/shared/footer.php
+++ development/www/wp-content/themes/starkers/parts/shared/footer.php
@@ -1,4 +1,4 @@
-	
-	<footer>
-		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
+
+	<footer class="layout-footer">
+		&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved. TEST
 	</footer>
--- development/www/wp-content/themes/starkers/parts/shared/header.php
+++ development/www/wp-content/themes/starkers/parts/shared/header.php
@@ -1,5 +1,5 @@
 
-<header>
+<header class="layout-header">
 	<h1><a href="/"><?php bloginfo( 'name' ); ?></a></h1>
 	<?php bloginfo( 'description' ); ?>
 	<?php get_search_form(); ?>
