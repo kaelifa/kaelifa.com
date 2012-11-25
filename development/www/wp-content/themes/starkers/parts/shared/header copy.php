62f78fa45af05c5dc64e43fca348bc647b9fb753
Kristina Auckland <kristina.auckland@gmail.com>
2012-11-25 19:59:12 +0000

    Fixed it (by deleting everything and starting again - still don't know what actually went wrong..) eep

---
+++ development/www/wp-content/themes/starkers/parts/shared/header copy.php
@@ -0,0 +1,13 @@
+
+<header class="layoutHeader">
+	<h1><a href="/"><?php bloginfo( 'name' ); ?></a></h1>
+	<?php bloginfo( 'description' ); ?>
+	<?php get_search_form(); ?>
+</header>
+<nav class="navPrimary">
+	<ul>
+		<!-- <li><a href="/">Blog</a></li> -->
+		<li class="current"><a href="index.html">About</a></li>
+		<li><a href="#contact" title="Contact">Contact</a></li>
+	</ul>
+</nav>
