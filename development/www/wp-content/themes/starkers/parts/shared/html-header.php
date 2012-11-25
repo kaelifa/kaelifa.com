3b7a083dd95c62485a88d9ef57940d0b1f232b65
Kristina Auckland <kristina.auckland@gmail.com>
2012-11-25 18:23:08 +0000

    Committing current state as I've broken it somewhere..! Bear with

--- development/www/wp-content/themes/starkers/parts/shared/html-header.php
+++ development/www/wp-content/themes/starkers/parts/shared/html-header.php
@@ -7,12 +7,18 @@
 	<head>
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="verify-v1" content="xFy1qUCTXX6nUsENpLEuyWWPzcXjYIpCzHXktf5rClI=" />
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
+
+		<script type="text/javascript" src="http://use.typekit.com/gfb2hkq.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
 		<!-- wp head start -->
 		<?php wp_head(); ?>
 		<!-- wp head end -->
 	</head>
 	<body <?php body_class(); ?>>
+
