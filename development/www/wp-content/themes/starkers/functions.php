2910bb83a9a3151e75e7edb88536c6d25c239da7
Kristina Auckland <kristina.auckland@gmail.com>
2012-11-25 20:39:41 +0000

    re-implementing starkers theme

--- development/www/wp-content/themes/starkers/functions.php
+++ development/www/wp-content/themes/starkers/functions.php
@@ -4,35 +4,35 @@
 	 *
 	 * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
 	 *
- 	 * @package 	WordPress
- 	 * @subpackage 	Starkers
- 	 * @since 		Starkers 4.0
+	 * @package 	WordPress
+	 * @subpackage 	Starkers
+	 * @since 		Starkers 4.0
 	 */
 
 	/* ========================================================================================================================
-	
+
 	Required external files
-	
+
 	======================================================================================================================== */
 
 	require_once( 'external/starkers-utilities.php' );
 
 	/* ========================================================================================================================
-	
+
 	Theme specific settings
 
 	Uncomment register_nav_menus to enable a single menu with the title of "Primary Navigation" in your theme
-	
+
 	======================================================================================================================== */
 
 	add_theme_support('post-thumbnails');
-	
+
 	// register_nav_menus(array('primary' => 'Primary Navigation'));
 
 	/* ========================================================================================================================
-	
+
 	Actions and Filters
-	
+
 	======================================================================================================================== */
 
 	add_action( 'wp_enqueue_scripts', 'script_enqueuer' );
@@ -40,19 +40,19 @@
 	add_filter( 'body_class', 'add_slug_to_body_class' );
 
 	/* ========================================================================================================================
-	
+
 	Custom Post Types - include custom post types and taxonimies here e.g.
 
 	e.g. require_once( 'custom-post-types/your-custom-post-type.php' );
-	
+
 	======================================================================================================================== */
 
 
 
 	/* ========================================================================================================================
-	
+
 	Scripts
-	
+
 	======================================================================================================================== */
 
 	/**
@@ -66,26 +66,27 @@
 		wp_register_script( 'site', get_template_directory_uri().'/js/site.js', array( 'jquery' ) );
 		wp_enqueue_script( 'site' );
 
-		wp_register_style( 'screen', get_template_directory_uri().'/style.css', '', '', 'screen' );
-        wp_enqueue_style( 'screen' );
-	}	
+		// wp_register_style( 'screen', get_template_directory_uri().'/style.css', '', '', 'screen' );
+		wp_register_style( 'screen', get_template_directory_uri().'/template/css/styles.css', '', '', 'screen' );
+		wp_enqueue_style( 'screen' );
+	}
 
 	/* ========================================================================================================================
-	
+
 	Comments
-	
+
 	======================================================================================================================== */
 
 	/**
-	 * Custom callback for outputting comments 
+	 * Custom callback for outputting comments
 	 *
 	 * @return void
 	 * @author Keir Whitaker
 	 */
 	function starkers_comment( $comment, $args, $depth ) {
-		$GLOBALS['comment'] = $comment; 
+		$GLOBALS['comment'] = $comment;
 		?>
-		<?php if ( $comment->comment_approved == '1' ): ?>	
+		<?php if ( $comment->comment_approved == '1' ): ?>
 		<li>
 			<article id="comment-<?php comment_ID() ?>">
 				<?php echo get_avatar( $comment ); ?>
--- development/www/wp-content/themes/starkers/index.php
+++ development/www/wp-content/themes/starkers/index.php
@@ -4,7 +4,7 @@
  * This is the most generic template file in a WordPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file 
+ * E.g., it puts together the home page when no home.php file
  *
  * Please see /external/starkers-utilities.php for info on get_template_parts()
  *
@@ -16,7 +16,7 @@
 <?php get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ): ?>
-<h2>Latest Posts</h2>	
+<h2>Latest Posts</h2>
 <ol>
 <?php while ( have_posts() ) : the_post(); ?>
 	<li>
