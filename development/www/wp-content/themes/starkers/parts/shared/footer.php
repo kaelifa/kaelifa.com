10e1b5281306c389205fed7f88a5e5af77457895
Kristina Auckland <kristina.auckland@gmail.com>
2012-12-02 13:04:50 +0000

    Added icon fonts and attempting to trial Yummo font

--- development/www/wp-content/themes/starkers/parts/shared/footer.php
+++ development/www/wp-content/themes/starkers/parts/shared/footer.php
@@ -24,14 +24,17 @@
 		<aside class="social">
 			<h3>Elsewhere</h3>
 			<ul>
-				<li><a href="http://www.twitter.com/kaelifa">Twitter</a></li>
-				<li><a href="http://www.linkedin.com/in/kaelifa">LinkedIn</a></li>
-				<li><a href="https://github.com/kaelifa">GitHub</a></li>
-				<li><a href="http://endorse.me/kaelifa">Endorse.me</a></li>
-				<li><a href="http://pinterest.com/kaelifa/">Pinterest</a></li>
-				<!-- <li><a href="http://zerply.com/kaelifa">Zerply</a></li>
-				<li><a href="http://www.behance.net/kaelifa">Behance</a></li>
-				<li><a href="http://dribbble.com/kaelifa">Dribbble</a></li> -->
+				<li><a class="icon icon1 icon-twitter" title="Twitter" href="http://www.twitter.com/kaelifa">t</a></li>
+				<li><a class="icon icon1 icon-linkedin" title="LinkedIn" href="http://www.linkedin.com/in/kaelifa">l</a></li>
+				<li><a class="icon icon2 icon-github" title="GitHub" href="https://github.com/kaelifa">j</a></li>
+				<!-- <li><a class="icon icon-endorse-me" title="Endorse.me" href="http://endorse.me/kaelifa">Endorse.me</a></li>
+				<li><a class="icon icon-pinterest" title="Pinterest" href="http://pinterest.com/kaelifa/">Pinterest</a></li> -->
+				<!--
+				<li><a class="icon icon1 icon-zerply" title="Zerply" href="http://zerply.com/kaelifa">z</a></li>
+				<li><a class="icon icon2 icon-behance" title="Behance" href="http://www.behance.net/kaelifa">G</a></li>
+				<li><a class="icon icon1 icon-dribbble" title="Dribbble" href="http://dribbble.com/kaelifa">d</a></li>
+				<li><a class="icon icon-colourlovers" title="colourlovers" href="http://www.colourlovers.com/lover/Fuchsia">ColourLovers</a></li>
+				-->
 			</ul>
 		</aside>
 		<div class="footerCopy">
